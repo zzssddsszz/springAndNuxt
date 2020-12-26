1354b89046d1b0eb6d414e2d091c458fd8497a35
방민준 <zzssddsszz@gmail.com>
2020-12-26 21:22:48 +0900

    인프라 컨피그 ing

---
+++ src/main/java/kr/co/iamdesigner/infrastructure/mail/AsyncMailer.java
@@ -0,0 +1,11 @@
+package kr.co.iamdesigner.infrastructure.mail;
+
+import kr.co.iamdesigner.domain.common.mail.Mailer;
+import kr.co.iamdesigner.domain.common.mail.Message;
+
+public class AsyncMailer implements Mailer {
+    @Override
+    public void send(Message message) {
+
+    }
+}
