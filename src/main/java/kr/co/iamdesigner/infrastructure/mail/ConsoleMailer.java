3297a1e401effcc58d16fc58c613f2b3d8122b11
방민준 <zzssddsszz@gmail.com>
2020-12-27 11:48:23 +0900

    인프라 컨피그 ing

---
+++ src/main/java/kr/co/iamdesigner/infrastructure/mail/ConsoleMailer.java
@@ -0,0 +1,18 @@
+package kr.co.iamdesigner.infrastructure.mail;
+
+import kr.co.iamdesigner.domain.common.mail.Mailer;
+import kr.co.iamdesigner.domain.common.mail.Message;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Profile({"local","test"})
+@Component
+public class ConsoleMailer implements Mailer {
+    @Override
+    public void send(Message message) {
+        log.info("메시지 제목 : ["+message.getSubject()+"]");
+        log.info("메시지 바디 : ["+message.getBody()+"]");
+    }
+}
