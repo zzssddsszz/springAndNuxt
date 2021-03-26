4bf586cd85244e9a396e4111594c296d4b2b9d17
minjun <zzssddsszz@gmail.com>
2021-03-26 22:08:11 +0900

    tiny editor 이미지 업로드 ~ing

--- admin/piaf-vue-start/src/components/Editor/TinyEditor.vue
+++ admin/piaf-vue-start/src/components/Editor/TinyEditor.vue
@@ -8,7 +8,7 @@
     :init="{
       height: 500,
       language: 'ko_KR',
-      images_upload_url: '/api/image',
+      images_upload_handler: example_image_upload_handler,
       plugins: [
         'advlist autolink link image lists charmap print preview hr anchor pagebreak',
         'searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking',
@@ -44,7 +44,54 @@ export default {
   methods: {
     change(){
       this.$emit("changeContent",this.content)
+    },
+
+    example_image_upload_handler (blobInfo, success, failure, progress) {
+      var xhr, formData;
+      console.log("이미지 핸들러 접근")
+
+      xhr = new XMLHttpRequest();
+      xhr.withCredentials = false;
+      xhr.open('POST', '/api/image');
+
+      xhr.upload.onprogress = function (e) {
+        progress(e.loaded / e.total * 100);
+      };
+
+      xhr.onload = function() {
+        var json;
+
+        if (xhr.status === 403) {
+          failure('HTTP Error: ' + xhr.status, { remove: true });
+          return;
+        }
+
+        if (xhr.status < 200 || xhr.status >= 300) {
+          failure('HTTP Error: ' + xhr.status);
+          return;
+        }
+
+        json = JSON.parse(xhr.responseText);
+
+        if (!json || typeof json.location != 'string') {
+          failure('Invalid JSON: ' + xhr.responseText);
+          return;
+        }
+
+        success(json.location);
+      };
+
+      xhr.onerror = function () {
+        failure('Image upload failed due to a XHR Transport error. Code: ' + xhr.status);
+      };
+
+      formData = new FormData();
+      formData.append('file', blobInfo.blob(), blobInfo.filename());
+
+      xhr.send(formData);
     }
   }
 }
+
+
 </script>
