48def8a00b02bd6bc3edeaaad53f3071449f8c37
방민준 <zzssddsszz@gmail.com>
2021-04-05 13:28:42 +0900

    회원가입 valid 테스트 불편해서 제거

--- src/main/java/com/modoodesigner/web/payload/RegistrationPayload.java
+++ src/main/java/com/modoodesigner/web/payload/RegistrationPayload.java
@@ -11,17 +11,17 @@ import javax.validation.constraints.Size;
 @Setter
 public class RegistrationPayload {
 
-    @Size(min = 4, max = 20, message = "유저이름은 4자 이상 20자 이하여야합니다.")
-    @NotNull
+//    @Size(min = 4, max = 20, message = "유저이름은 4자 이상 20자 이하여야합니다.")
+//    @NotNull
     private String username;
 
-    @Email
-    @Size(max = 100, message = "이메일은 100자 이내여야 합니다.")
-    @NotNull
+//    @Email
+//    @Size(max = 100, message = "이메일은 100자 이내여야 합니다.")
+//    @NotNull
     private String emailAddress;
 
-    @Size(min = 6, max = 30, message = "비밀번호는 6자 이상 30자 이하여야합니다.")
-    @NotNull
+//    @Size(min = 6, max = 30, message = "비밀번호는 6자 이상 30자 이하여야합니다.")
+//    @NotNull
     private String password;
 
     public UserRegisterCommand toCommand() {
